538f12a2275bb68dfe2469079f3fbd3dfba7a98c
Steve Diabo <stevediabo@gmail.com>
2021-07-07 11:58:00 -0400

    big old cleanup

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,29 @@ export default defineComponent({
 html {
 	height: 100%;
 	overflow: hidden;
+	touch-action: none;
 }
 
 body {
 	background-color: #000;
+	background-image: 
+		url("/assets/bg-landscape-2.png"),
+		url("/assets/change-camera-landscape.png"),
+		url("/assets/change-camera-portrait.png"),
+		url("/assets/shutter-landscape.png"),
+		url("/assets/shutter-portrait.png"),
+		url("/assets/timer-disabled-landscape.png"),
+		url("/assets/timer-disabled-portrait.png"),
+		url("/assets/timer-enabled-landscape.png"),
+		url("/assets/timer-enabled-portrait.png"),
+		url("/assets/timer.gif"),
+		url("/assets/viewfinder-glass-landscape.png"),
+		url("/assets/viewfinder-glass-portrait.png"),
+		url("/assets/viewfinder-vignette-landscape.png"),
+		url("/assets/viewfinder-vignette-portrait.png");
+	background-position: -1000% -1000%;
+	background-repeat: no-repeat;
+	background-size: 0;
 	color: #fff;
 	width: 100%;
 	height: 100%;
