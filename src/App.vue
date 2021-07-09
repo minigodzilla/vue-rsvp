3ac7bf0455d512e0984cc51bed2a583621def23d
Steve Diabo <stevediabo@gmail.com>
2021-07-09 19:38:23 -0400

    prettier

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-	<!-- <h1>Jenn and Steve's Wedding RSVP</h1> -->
 	<Camera />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Camera from "./components/Camera.vue";
+import { defineComponent } from 'vue';
+import Camera from './components/Camera.vue';
 
 export default defineComponent({
-	name: "App",
+	name: 'App',
 	components: {
 		Camera,
 	},
@@ -24,21 +23,7 @@ html {
 
 body {
 	background-color: #000;
-	background-image: 
-		url("/assets/bg-landscape-2.png"),
-		url("/assets/change-camera-landscape.png"),
-		url("/assets/change-camera-portrait.png"),
-		url("/assets/shutter-landscape.png"),
-		url("/assets/shutter-portrait.png"),
-		url("/assets/timer-disabled-landscape.png"),
-		url("/assets/timer-disabled-portrait.png"),
-		url("/assets/timer-enabled-landscape.png"),
-		url("/assets/timer-enabled-portrait.png"),
-		url("/assets/timer.gif"),
-		url("/assets/viewfinder-glass-landscape.png"),
-		url("/assets/viewfinder-glass-portrait.png"),
-		url("/assets/viewfinder-vignette-landscape.png"),
-		url("/assets/viewfinder-vignette-portrait.png");
+	background-image: url('/assets/bg-landscape-2.png'), url('/assets/change-camera-landscape.png'), url('/assets/change-camera-portrait.png'), url('/assets/shutter-landscape.png'), url('/assets/shutter-portrait.png'), url('/assets/timer-disabled-landscape.png'), url('/assets/timer-disabled-portrait.png'), url('/assets/timer-enabled-landscape.png'), url('/assets/timer-enabled-portrait.png'), url('/assets/timer.gif'), url('/assets/viewfinder-glass-landscape.png'), url('/assets/viewfinder-glass-portrait.png'), url('/assets/viewfinder-vignette-landscape.png'), url('/assets/viewfinder-vignette-portrait.png');
 	background-position: -1000% -1000%;
 	background-repeat: no-repeat;
 	background-size: 0;
